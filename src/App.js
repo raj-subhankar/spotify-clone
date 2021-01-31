f00927b3f8d54e3d89b8985ca306857444798ce7
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-31 06:58:21 +0530

    Implement Spotify login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+
+  const [token, setToken] = useState(null)
+
+    useEffect(() => {
+        const hash = getTokenFromUrl()
+        window.location.hash = ""
+
+        const _token = hash.access_token
+
+        if(_token) {
+            setToken(_token)
+            spotify.setAccessToken(_token)
+        }
+
+    }, [])
+
   return (
     <div className="app">
-      
+      { token ? <h1> I am Logged in </h1> : <Login />}
     </div>
   );
 }
